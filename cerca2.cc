f15bbcc20c16fbdab7bbdc2225d9b28cb65324c0
guilleemguil <guillem.guilera@estudiantat.upc.edu>
2024-11-25 11:26:13 +0100

    Update cerca2.cc

--- cerca2.cc
+++ cerca2.cc
@@ -15,14 +15,28 @@ struct Coordinates{
     int y;
 };
 
-typedef vector< vector<int> > FR;
+struct Position{
+    Coordinates ul;
+    Coordinates br;
+};
+
+typedef vector<vector<bool>> FR;
 int N, W;
 vector<Order> Orders;
 
 
 // insereix la peça de 'Order' a la coordenada actual coord (del dret o del revès).
 // Marca fabric_roll indicant que allà hem posat la peça
-int insert_new_piece(Order order, FR& fabric_roll, Coordinates coord, bool reverse){
+void insert_new_piece(vector<Position> p_sol, Order order, FR& fabric_roll, Coordinates coord, int c, bool reverse){
+    int x = coord.x; int y = coord.y;
+    int w = order.width; int h = order.height;
+    if (reverse) w, h = h, w;
+    p_sol[c] = Position{Coordinates{x, y}, Coordinates{x + w, y + h}};
+    for (int i = x; i < x + w; ++i){
+        for(int j = y; j < y + h; ++j){
+            fabric_roll[i][j] = true;
+        }
+    }
 }
 
 // mira si la nova peça cap a la coordenada actual (li podem dir que provi la peça del revès)
@@ -39,8 +53,8 @@ void delete_piece(FR fabric_roll, Order order, Coordinates coord, bool reverse){
 }
 
 
-bool exhaustive_search_solution_recursive(FR& fabric_roll, int count, Coordinates current_coord, 
-                                        int current_L, int best_L ){
+void exhaustive_search_solution_recursive(FR& fabric_roll, int count, Coordinates current_coord, 
+                                        int current_L, int best_L, vector<Position> p_sol){
     if (count == N) {
         if (current_L < best_L) best_L = current_L;
         // find_best_positions
@@ -54,16 +68,20 @@ bool exhaustive_search_solution_recursive(FR& fabric_roll, int count, Coordinate
                     // si no està ocupada:
                     if (piece_fits(fabric_roll, order, current_coord, false)) {
                         // la posem del dret
-                        insert_new_piece(order, fabric_roll, current_coord, false);
-                        current_coord = set_new_coordinates(fabric_roll, current_coord); 
-                        exhaustive_search_solution_recursive(fabric_roll, count + 1, current_coord, current_L, best_L);
+                        insert_new_piece(p_sol, order, fabric_roll, current_coord, count, false);
+                        current_L = max(current_L, current_coord.y + order.height + 1);
+                        exhaustive_search_solution_recursive(fabric_roll, count + 1,
+                                                             set_new_coordinates(fabric_roll, current_coord),
+                                                             current_L, best_L, p_sol);
                         delete_piece(fabric_roll, order, current_coord, false); // eliminem la peça per trobar una nova combinació
                     }
-                    if (piece_fits(fabric_roll, order, current_coord, true)){
+                    if (order.width != order.height && piece_fits(fabric_roll, order, current_coord, true)){
                         // la posem del revès
-                        insert_new_piece(order, fabric_roll, current_coord, true);
-                        current_coord = set_new_coordinates(fabric_roll, current_coord);
-                        exhaustive_search_solution_recursive(fabric_roll, count + 1, current_coord, current_L, best_L);
+                        insert_new_piece(p_sol, order, fabric_roll, current_coord, count, true);
+                        current_L = max(current_L, current_coord.y + order.width + 1);
+                        exhaustive_search_solution_recursive(fabric_roll, count + 1,
+                                                             set_new_coordinates(fabric_roll, current_coord),
+                                                             current_L, best_L, p_sol);
                         delete_piece(fabric_roll, order, current_coord, true); // eliminem la peça per trobar una nova combinació
                     }
                 }
@@ -89,7 +107,7 @@ int main() {
     L_max = 0;
     while (cin >> n_units) {
         cin >> p >> q;
-        Orders.push_back({n_units, p, q});
+        Orders.push_back(Order{n_units, p, q});
         L_max += q;
     }
     exhaustive_search_solution();
