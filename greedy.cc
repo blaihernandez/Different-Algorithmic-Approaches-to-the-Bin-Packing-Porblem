7ee3bc596044e298b9e31d873815d415ec958170
guilleemguil <guillem.guilera@estudiantat.upc.edu>
2024-11-30 17:44:50 +0100

    greedy amb comprovacions i versio 1

--- greedy.cc
+++ greedy.cc
@@ -1,13 +1,14 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <algorithm>
 
 using namespace std;
 
 struct Order {
     int n; /* nombre de Order de tipus (width x height)*/
-    int width;
-    int height;
+    int p;
+    int q;
 };
 
 struct Coordinates{
@@ -21,10 +22,10 @@ struct Position{
 };
 
 typedef vector<vector<bool>> FR;
-int N, W;
-vector<Order> Orders;
+int N, W, L_max;
+vector<Order> orders;
 
-void escriu_matriu(vector<vector<bool>> matriu) {
+void escriu(vector<vector<int>> matriu) {
     for(int i = 0; i < matriu.size(); ++i) {
         for(int j = 0; j < matriu[0].size(); ++j) {
             cout << matriu[i][j] << " ";
@@ -34,15 +35,66 @@ void escriu_matriu(vector<vector<bool>> matriu) {
     cout << endl;
 }
 
+void ordenar_rectangles(vector<Order>& orders) {
+    sort(orders.begin(), orders.end(), [](const Order& a, const Order& b) {
+        return a.p * a.q > b.p * b.q;
+    });
+}
+
+bool cap(int dx, int dy, int x, int y, vector<vector<int>>& teler){
+    if(dx + x > W) return false;
+    for (int i = y; i < y + dy; ++i){
+        for (int j = x; j < x + dx; ++j) if(teler[i][j] != 0) return false;
+    }
+    return true;
+}
+
+void poso(int dx, int dy, int x, int y, vector<vector<int>>& teler, int id){
+    for (int i = y; i < y + dy; ++i){
+        for (int j = x; j < x + dx; ++j) teler[i][j] = id;
+    }
+}
+
+void greedy(){
+    ordenar_rectangles(orders);
+    int id = 0; int L = 0;
+    vector<vector<int>> teler(L_max, vector<int>(W, 0));
+    for (Order r : orders){
+        for(int q = 0; q < r.n; ++q){
+            ++id;
+            cout << "rectangle " << r.p << r.q << endl; //treure
+            bool posat = false;
+            for(int y = 0; y < L_max and not posat; ++y){
+                for(int x = 0; x < W and not posat; ++x){
+                    if (cap(r.q, r.p, x, y, teler)){
+                        cout << "cap H" << endl; //treure
+                        poso(r.q, r.p, x, y, teler, id);
+                        escriu(teler); //treure
+                        posat = true;
+                        L = max(L, y + r.p);
+                    }
+                    else if (cap(r.p, r.q, x, y, teler)){
+                        cout << "cap V" << endl; //treure
+                        poso(r.p, r.q, x, y, teler, id);
+                        escriu(teler); //treure
+                        posat = true;
+                        L = max(L, y + r.q);
+                    }
+                }
+            }
+        }
+    }
+    cout << L << endl;
+}
+
 int main() {
-    int n_units, p, q, L_max;
+    int n_units, p, q;
     cin >> W >> N;
     L_max = 0;
     while (cin >> n_units) {
         cin >> p >> q;
-        Orders.push_back(Order{n_units, p, q});
-        L_max += q;
+        orders.push_back(Order{n_units, p, q});
+        L_max += q * n_units;
     }
-    //sort_orders(Orders);
-    exhaustive_search_solution(L_max);
+    greedy();
 }
